9769e380867545190493c1387e1779c60c1d4b18
Joakoxdhalo <joacojimeca@gmail.com>
2019-08-29 16:53:27 -0500

    * Add my first PHP file: Only has print functions

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+		print "Prueba de impresión 1 <br>"; //este es un comentario
+		print "Prueba de impresión 2 <br>";
+		print "Prueba de impresión 3"
+		/* De la siguiente manera puedo hacer
+		comentarios que son largos en PHP
+		se diferencian con los comentarios en Python
+		en el # y en Django por {% comment %} 
+		*/
+	?>
+</body>
+</html>
